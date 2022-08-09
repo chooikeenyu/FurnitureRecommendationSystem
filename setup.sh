0640d8e55ed2e4d6364a7b91957a2d260bcd2bb1
Chooi Keen Yu <55182537+chooikeenyu@users.noreply.github.com>
2022-08-09 21:17:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"chooikeenyu@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "[general]  
+email = \"chooikeenyu@gmail.com\""  > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false"  >> ~/.streamlit/config.toml
